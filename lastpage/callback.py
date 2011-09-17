13bd2ee48a0489ed40f06cba10cbd8f5575169ae
terrycojones <terry@fluidinfo.com>
2011-09-16 21:51:47 -0400

    Use the Agent to call verify credentials (via OAuth Echo) so we can see details of any error.

--- lastpage/callback.py
+++ lastpage/callback.py
@@ -15,11 +15,16 @@
 import json
 import uuid
 
+from lastpage.agent import ResponseConsumer
+
 from oauth.oauth import (
     OAuthToken, OAuthRequest, OAuthConsumer, OAuthSignatureMethod_HMAC_SHA1)
 
+from twisted.internet import reactor
 from twisted.python import log
 from twisted.web import resource, client, server
+from twisted.web.client import Agent
+from twisted.web.http_headers import Headers
 
 
 class Callback(resource.Resource):
@@ -30,17 +35,22 @@ class Callback(resource.Resource):
     @param oauthTokenDict: A C{dict} mapping OAuth keys to tokens.
     @param conf: A L{config.Config} instance holding configuration
         settings.
+    @param agent: The C{twisted.web.client.Agent} instance to use when making
+        HTTP requests to the API endpoint.
     """
     isLeaf = True
 
-    def __init__(self, cookieDict, oauthTokenDict, conf):
+    def __init__(self, cookieDict, oauthTokenDict, conf, agent=None):
         self._conf = conf
         self._cookieDict = cookieDict
         self._oauthTokenDict = oauthTokenDict
+        self._agent = agent or Agent(reactor)
 
     def render_GET(self, request):
         """
         Handles a callback GET request.
+
+        @param request: A twisted.web HTTP C{Request}.
         """
         log.err('Callback received: %s' % request)
 
@@ -79,6 +89,14 @@ class Callback(resource.Resource):
         return server.NOT_DONE_YET
 
     def _storeAccessToken(self, result, request):
+        """Store a Twitter access token and begin a verify credentials call.
+
+        @param result: A C{str} containing a Twitter access token.
+        @param request: A twisted.web HTTP C{Request}.
+        @return: A C{Deferred} that will fire with the result of calling
+            verify credentials (or an OAuth Echo endpoint which will proxy
+            the verify credentials request.
+        """
         accessToken = OAuthToken.from_string(result)
         log.msg('Got access token: %s' % accessToken)
         conf = self._conf
@@ -94,18 +112,73 @@ class Callback(resource.Resource):
             # VerifyCredentials ourselves directly.
             authHeader = oaRequest.to_header(
                 conf.verify_credentials_url)['Authorization']
-            headers = {
-                'X-Auth-Service-Provider': conf.verify_credentials_url,
-                'X-Verify-Credentials-Authorization': authHeader,
-                }
-            d = client.getPage(conf.oauth_echo_url, headers=headers)
+            headers = Headers({
+                'X-Auth-Service-Provider': [conf.verify_credentials_url],
+                'X-Verify-Credentials-Authorization': [authHeader],
+                })
+            url = conf.oauth_echo_url
         else:
-            d = client.getPage(oaRequest.to_url())
-        d.addCallback(self._storeUser, accessToken, request)
+            headers = Headers()
+            url = oaRequest.to_url()
+        d = self._agent.request('GET', url, headers)
+        d.addCallbacks(self._consumeResponse, self._verifyCredentialsError,
+                       callbackArgs=[accessToken, request],
+                       errbackArgs=[request])
         d.addErrback(log.err)
         return d
 
+    def _verifyCredentialsError(self, fail, request):
+        """
+        Credentials could not be verified. Log the error and redirect the
+        user to the failed login URL.
+
+        @param fail: a twisted.python C{Failure} instance.
+        @param request: A twisted.web HTTP C{Request}.
+        """
+        log.msg('Agent could not set up connection for verify credentials:')
+        log.err(fail.value)
+        log.msg('Redirect to %s' % self._conf.login_failure_redirect_url)
+        request.redirect(self._conf.login_failure_redirect_url)
+        request.finish()
+
+    def _consumeResponse(self, response, accessToken, request):
+        """Read HTTP response data from the verify credentials call.
+
+        If the response is successful (code 200), arrange for the body to be
+        retrieved and delivered to self._storeUser. If not, log an error
+        including any X-FluidDB headers, and redirect the user to the
+        login failed URL.
+
+        @param response: A twisted.web.client C{Response}.
+        @param accessToken: A C{str} access token from Twitter.
+        @param request: A twisted.web HTTP C{Request}.
+        """
+        if response.code == 200:
+            consumer = ResponseConsumer()
+            consumer.deferred.addCallback(self._storeUser, accessToken,
+                                          request)
+            response.deliverBody(consumer)
+            return consumer.deferred
+        else:
+            log.msg('Received non-200 %d status (%s) on verify credentials '
+                    'call.' % (response.code, response.phrase))
+            for header, value in response.headers.getAllRawHeaders():
+                if header.lower().startswith('x-fluiddb'):
+                    log.msg('Response header %r = %r.' % (header, value))
+            log.msg('Redirect to %s' % self._conf.login_failure_redirect_url)
+            request.redirect(self._conf.login_failure_redirect_url)
+            request.finish()
+
     def _storeUser(self, result, accessToken, request):
+        """Decode the user data from verify credentials and store it
+        locally.  Push a cookie to the client that will let us find the
+        login information when they return.  Redirect the request to the
+        logged in page.
+
+        @param result: the JSON C{str} from the verify credentials call.
+        @param accessToken: A C{str} access token from Twitter.
+        @param request: A twisted.web HTTP C{Request}.
+        """
         user = json.loads(result)
         key = str(uuid.uuid4())
         conf = self._conf
